7f91c9105e28aba1cbb65e8adf472c51069c61f4
Aman Sharma <>
2023-07-27 11:23:46 +0530

    - changed baseUrl

--- src/utils/application.properties.js
+++ src/utils/application.properties.js
@@ -1,4 +1,4 @@
 export const applicationProperties = {
-  baseUrl: 'https://glue.de.faas-suite-prod.cloud.spryker.toys/',
-  // baseUrl: 'http://glue.us.spryker.local/',
+  // baseUrl: 'https://glue.de.faas-suite-prod.cloud.spryker.toys/',
+  baseUrl: 'http://glue.us.spryker.local/',
 };
